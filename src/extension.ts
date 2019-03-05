0a1772919ef731e5f3157c0fb22567cb16533fbf
Alberto Vélaz <albertovelaz.dev@gmail.com>
2019-03-05 19:36:28 +0100

    Add ability for selecting several rows at the same time

--- src/extension.ts
+++ src/extension.ts
@@ -6,46 +6,44 @@ const transformTimeForHumans = () => {
 	const editor = vscode.window.activeTextEditor;
 	const text = editor ? editor.document.getText(editor.selection) : undefined;
 	
-
 	if (editor && text) {
-
-		// console.log(editor);
-		// console.log('lines => ', editor.selection.start.line - editor.selection.end.line + 1);
-
-		// const lines = editor.selection.start.line - editor.selection.end.line + 1;
+		const lines = editor.document.getText().split('\n');
+		let selectedLines = lines.slice(editor.selection.start.line, editor.selection.end.line + 1);
+		
 		const settingsFormat = vscode.workspace.getConfiguration('timeForHumans').format;
-		// console.log(settingsFormat);
-		//TODO Add loop in lines for adding dates in necessary ones
-		const timestamp : number = parseInt(text);
-		// var myDate : Date = new Date( timestamp * 1000);
-		var length : number = Math.log(timestamp) * Math.LOG10E + 1 | 0;
-		var difference : number = ((13 - length) < 0) ?  0 : 13 - length; 
-		var factor : number = factor = 10 ** difference;
-		var myDate : string = moment(timestamp * factor).format(settingsFormat);
-		const lang = editor.document.languageId;
-		let commentStart : string = '';
-		let commentEnd: string = '';
-		switch(lang) {			
-			case 'csharp':			
-			case 'go':
-			case 'java':			
-			case 'php':
-			case 'plaintext':
-			case 'typescript':
-				commentStart = ' /* ';
-				break;
-			case 'javascript':
-				commentStart = ' // ';
-			case 'python':
-			case 'ruby':
-				commentStart = ' # ';
-				break;
-			default:
-				commentStart = '';
-				break;
-		}
-		//const newText = text + commentStart + myDate.toUTCString() + "  => Locale Time " + myDate.toLocaleString() + commentEnd;
-		const newText = text + commentStart + ' ' + myDate + ' ' + commentEnd;
+		let newText = '';
+		selectedLines.forEach((line, index) => {
+			const timestamp : number = parseInt(line);
+			var length : number = Math.log(timestamp) * Math.LOG10E + 1 | 0;
+			var difference : number = ((13 - length) < 0) ?  0 : 13 - length; 
+			var factor : number = factor = 10 ** difference;
+			var myDate : string = moment(timestamp * factor).format(settingsFormat);
+			const lang = editor.document.languageId;
+			let commentStart : string = '';
+			let commentEnd: string = '';
+			switch(lang) {			
+				case 'csharp':			
+				case 'go':
+				case 'java':			
+				case 'php':
+				case 'plaintext':
+				case 'typescript':
+					commentStart = ' /* ';
+					commentEnd = ' */';
+					break;
+				case 'javascript':
+					commentStart = ' // ';
+				case 'python':
+				case 'ruby':
+					commentStart = ' # ';
+					break;
+				default:
+					commentStart = '';
+					break;
+			}
+			newText += line + commentStart + ' ' + myDate + ' ' + commentEnd;
+			newText += (selectedLines.length - 1 > index) ? '\n' : '';
+		});
 		
 		editor.edit(builder => builder.replace(editor.selection, newText));	
 	}
@@ -54,7 +52,6 @@ const transformTimeForHumans = () => {
 export function activate(context: vscode.ExtensionContext) {
 
 	// console.log('Congratulations, your extension "time-for-humans" is now active!');
-
 	let disposable = vscode.commands.registerCommand('extension.humanTime', () => {
 		transformTimeForHumans();
 	});
@@ -62,5 +59,4 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
